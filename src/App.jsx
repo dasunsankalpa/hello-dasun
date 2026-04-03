5d30bbf43702b68a22f1e54a0a90842e32c2048e
dasunsankalpa <dasunsankalpapathirana@gmail.com>
2026-04-03 13:38:37 +0530

    Set WhatsApp image as background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    document.body.classList.add('bg-whatsapp');
+    return () => {
+      document.body.classList.remove('bg-whatsapp');
+    };
+  }, []);
   return (
     <div>
       <h1>Hello Dasun</h1>
